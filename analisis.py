ff2dc4f612a55bdff6c4a1528fac727bdfed5bce
Conrado Martinez <conrado@conradoymas.com>
2026-06-27 09:58:47 +0200

    Prueba en la rama nueva-funcion

--- analisis.py
+++ analisis.py
@@ -2,4 +2,4 @@ print("Analizando datos")
 print("Python es fantástico")
 print("Añadida una nueva funcionalidad")
 print("Calculando estadísticas...")
-
+print("Estoy trabajando en la rama nueva-funcion")
